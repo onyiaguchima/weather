67241c6345f0a580a37575a6c397d73fac935308
Chima <onyiaguchima@gmail.com>
2022-09-02 12:37:49 +0100

    second commit

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ let url = "https://blockchain.info/ticker"
 request({url: url}, (error, response)=>{
     let data = JSON.parse(response.body)
     console.log(data.USD)
-    console.log(error, "e")
+    console.log(error, "error")
 });
 
