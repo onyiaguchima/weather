fd1f035386028b957f11f1c376bb483b83dce126
Chima <onyiaguchima@gmail.com>
2022-09-02 21:11:54 +0100

    addition to the data

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log(request);
 let url = "https://blockchain.info/ticker"
 request({url: url}, (error, response)=>{
     let data = JSON.parse(response.body)
-    console.log(data.USD)
+    console.log(data.USD, "this data")
     console.log(error, "error")
 });
 
